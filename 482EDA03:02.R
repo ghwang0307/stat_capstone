4ceb75493899e0db4625f02234775ed7d6474729
MaliSelman <79944951+MaliSelman@users.noreply.github.com>
2021-03-10 18:46:08 -0600

    Update 482EDA03:02.R

--- 482EDA03:02.R
+++ 482EDA03:02.R
@@ -1,5 +1,3 @@
-library(astsa)
-# package
 variable.names(TrafficData)
 TrafficData <- data.frame(TrafficData)
 TrafficData[,1]
@@ -8,52 +6,70 @@ par(mfrow = c(2,1))
 time = TrafficData[,2]
 sens1 = TrafficData[,3]
 tsplot(time,sens1)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average1 = mean(TrafficData[["S313368"]], na.rm = T)
+average1
+abline(h = average1, col = "red")
 
 ##work with time vs. sensor 2
-time = TrafficData[,2]
 sens2 = TrafficData[,4]
 tsplot(time,sens2)
-average = mean(TrafficData[["S31337"]], na.rm = T)
-abline(h = average, col = "red")
+average2 = mean(TrafficData[["S313376"]], na.rm = T)
+average2
+abline(h = average2, col = "red")
+
 ##work with time vs. sensor 3
-sens3 = TrafficData[,5]
+sens2 = TrafficData[,5]
 tsplot(time,sens3)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average3 = mean(TrafficData[["S313487"]], na.rm = T)
+average3
+abline(h = average3, col = "red")
+
 ##work with time vs. sensor 4
 sens4 = TrafficData[,6]
 tsplot(time,sens4)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average4 = mean(TrafficData[["S314165"]], na.rm = T)
+average4
+abline(h = average4, col = "red")
+
 ##work with time vs. sensor 5
 sens5 = TrafficData[,7]
 tsplot(time,sens5)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average5 = mean(TrafficData[["S314402"]], na.rm = T)
+average5
+abline(h = average5, col = "red")
+
 ##work with time vs. sensor 6
 sens6 = TrafficData[,8]
 tsplot(time,sens6)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average6 = mean(TrafficData[["S314514"]], na.rm = T)
+average6
+abline(h = average6, col = "red")
+
 ##work with time vs. sensor 7
 sens7 = TrafficData[,9]
 tsplot(time,sens7)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average7 = mean(TrafficData[["S316063"]], na.rm = T)
+average7
+abline(h = average7, col = "red")
+
 ##work with time vs. sensor 8
 sens8 = TrafficData[,10]
 tsplot(time,sens8)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average8 = mean(TrafficData[["S316387"]], na.rm = T)
+average8
+abline(h = average8, col = "red")
+
 ##work with time vs. sensor 9
 sens9 = TrafficData[,11]
 tsplot(time,sens9)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average9 = mean(TrafficData[["S318586"]], na.rm = T)
+average9
+abline(h = average9, col = "red")
+
 ##work with time vs. sensor 10
 sens10 = TrafficData[,12]
 tsplot(time,sens10)
-average = mean(TrafficData[["S313368"]], na.rm = T)
-abline(h = average, col = "red")
+average10 = mean(TrafficData[["S318593"]], na.rm = T)
+average10
+abline(h = average10, col = "red")
+
